fd9dd3f1c24ad4a8d03a85f4a3e93b1318b3da17
arshahdul99 <Arshahdul99@gmail.com>
2022-07-03 17:59:53 +0530

    Uploaded

---
+++ Educational-Website-Codefest/app.js
@@ -0,0 +1,12 @@
+//This Will Toggle Nav-Links:
+function showNav(){
+    document.getElementById('links').style.display = "block";
+    document.getElementById('on').style.display = "none";
+    document.getElementById('off').style.display = "block";
+};
+//This Will Hide Nav-links:
+function closeNav(){
+    document.getElementById('links').style.display = "none";
+    document.getElementById('on').style.display = "block";
+    document.getElementById('off').style.display = "none";
+}
